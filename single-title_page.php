bc69608ee677dd2ed9e237cb980650f8d3798373
FrameofMind <FrameofMind1701@gmail.com>
2014-10-17 19:55:50 -0500

    Fixed ind. page sidebars (partly)
    Streamlined the page/post object linking for the individual page
    sidebars -- still need to add a default loop for when no posts have been
    chosen manually.

--- single-title_page.php
+++ single-title_page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The Template for displaying all Person Page type posts.
+ * The Template for displaying all Title Page type posts.
  *
  * @package hownottobeseen
  */
@@ -14,33 +14,6 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 		
 		<aside class="featured-works">
-				<?php if( get_field( 'related_titles' ) ): ?>	
-				<h1>Featured Works</h1>
-				<ul>
-						<?php while( has_sub_field( 'related_titles' ) ): ?>
-							<?php
-							$post_object = get_sub_field('page_link');
-
-								if( $post_object ): 
-									// override $post
-									$post = $post_object;
-									setup_postdata( $post );
-							?>
-								<li>
-									<a class="title-link" href="<?php the_permalink(); ?>">
-										<h2><?php the_title(); ?></h2>
-										<div class="masked-image-wrapper">
-											<div class="image-mask"></div>
-											<?php the_post_thumbnail(); ?>
-										</div>
-									</a>
-								</li>
-							<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-							<?php endif; ?>
-						<?php endwhile; ?>
-					<?php endif; // end of repeater loop. ?>
-				</ul>
-
 				<?php if( get_field( 'related_people' ) ): ?>	
 				<h1>Related People</h1>
 				<ul>
@@ -67,7 +40,35 @@ get_header(); ?>
 						<?php endwhile; ?>
 					<?php endif; // end of repeater loop. ?>
 				</ul>
+				
+				<?php if( get_field( 'related_titles' ) ): ?>	
+				<h1>Related Titles</h1>
+				<ul>
+						<?php while( has_sub_field( 'related_titles' ) ): ?>
+							<?php
+							$post_object = get_sub_field('page_link');
+
+								if( $post_object ): 
+									// override $post
+									$post = $post_object;
+									setup_postdata( $post );
+							?>
+								<li>
+									<a class="title-link" href="<?php the_permalink(); ?>">
+										<h2><?php the_title(); ?></h2>
+										<div class="masked-image-wrapper">
+											<div class="image-mask"></div>
+											<?php the_post_thumbnail(); ?>
+										</div>
+									</a>
+								</li>
+							<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+							<?php endif; ?>
+						<?php endwhile; ?>
+					<?php endif; // end of repeater loop. ?>
+				</ul>
 		</aside>
+				
 		
 		
 		
@@ -79,43 +80,25 @@ get_header(); ?>
 			</header>
 			
 			<ul class="vital-statistics">
-				<li><strong>Date of Birth:</strong> <?php the_field( 'date_of_birth' ); ?></li>
-				<li><strong>Most Famous For:</strong> <?php the_field( 'most_famous_for' ); ?></li>
+				<?php if( get_field( 'year' ) ): ?>
+				<li><strong>Year:</strong> <?php the_field( 'year' ); ?></li>
+				<?php endif; // end of repeater loop. ?>
+				
+				<?php if( get_field( 'people' ) ): ?>
+				<li><strong>Cast:</strong>
+					<?php while( has_sub_field( 'people' ) ): ?>
+						<span><?php the_sub_field( 'person' ); ?></span>
+						<span>&nbsp;/&nbsp;</span>
+					<?php endwhile; ?>
+				</li>
+				<?php endif; // end of repeater loop. ?>
 			</ul>
-					
-			<h2>My Thoughts</h2>
+			
 			<?php get_template_part( 'content' ); ?>
 			
-			<h2>Strengths</h2>
-				<?php the_field( 'strengths' ); ?>
-					
-			<h2>Weaknesses</h2>
-				<?php the_field( 'weaknesses' ); ?>
 			
-			<h2>Filmography</h2>
-				<table class="filmography">
-						<tr>
-							<th>Year</th>
-							<th>Medium</th>
-							<th>Title</th>
-							<th>My Rating</th>
-							<th>Character</th>
-							<th>Role Type</th>
-						</tr>
-						
-				<?php if( get_field('filmography') ): ?>
-					<?php while( has_sub_field('filmography') ): ?>
-						<tr>
-							<td class="year"><?php the_sub_field('year'); ?></td>
-							<td class="medium"><?php the_sub_field('medium'); ?></td>
-							<td class="title"><?php the_sub_field('title'); ?></td>
-							<td class="rating"><?php the_sub_field('my_rating'); ?></td>
-							<td class="character"><?php the_sub_field('character'); ?></td>
-							<td class="role"><?php the_sub_field('role_type'); ?></td>
-						</tr>
-					<?php endwhile; ?>
-				<?php endif; // end of filmography repeater loop. ?>
-				</table><!-- .filmography -->
+			
+			
 			
 
 		<?php endwhile; // end of the loop. ?>
